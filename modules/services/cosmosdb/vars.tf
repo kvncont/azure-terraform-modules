161634b0e7320b4d2fcf7526a7c53a0ddd5f3e6e
Kevin Contreras <kvncont@gmail.com>
2021-07-27 09:01:19 -0600

    Docs: update cosmosdb readme

--- modules/services/cosmosdb/vars.tf
+++ modules/services/cosmosdb/vars.tf
@@ -1,20 +1,24 @@
-
 variable "cosmosdb_account_name" {
+  type        = string
   description = "Name cosmos account"
 }
 
 variable "cosmosdb_database_name" {
+  type        = string
   description = "Name cosmos database"
 }
 
 variable "resource_group_name" {
+  type        = string
   description = "Resource group name"
 }
 
 variable "location" {
+  type        = string
   description = "Resource location"
 }
 
 variable "tags" {
+  type        = map(string)
   description = "Resource tags"
 }
